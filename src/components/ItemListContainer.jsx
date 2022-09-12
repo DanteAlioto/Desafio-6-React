bafee70dbf014a899183d57b12521d5df0d4f3d0
Dante <dantealioto@yahoo.it>
2022-09-12 18:08:19 -0300

    correciones

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import ItemList from "./ItemList";
-import ItemCount from "./ItemCount";
 import products from "./products";
 
 const ItemListContainer = () => {
